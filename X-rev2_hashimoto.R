733d15190352f6dad3a6d943e9238e0215cab381
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-08-25 18:16:37 -0400

    Script to examine how common increasing RH trends are
    Ah, Reviewer 1

---
+++ X-rev2_hashimoto.R
@@ -0,0 +1,75 @@
+# Test: how many grid cells show positive, and SIGNIFICANT positive, RH trends?
+# This is to address Reviewer 1's concern that our site- and FLUXNET-specific
+# show inconsistent results that we don't fully explain.
+#
+# Method: load Hashimoto (2015) dataset; apply a linear model to the 1990-2012 data 
+# for each global grid cell; extract slope and significance information and report them.
+#
+# Ben Bond-Lamberty August 2017
+
+source("0-functions.R")
+
+SCRIPTNAME  	<- "X-rev2_hashimoto.R"
+PROBLEM       <- FALSE
+
+
+# ==============================================================================
+# Main 
+
+openlog(file.path(outputdir(), paste0(SCRIPTNAME, ".log.txt")), sink = TRUE)
+printlog("Welcome to", SCRIPTNAME)
+
+library(ncdf4)
+# Downloaded August 25, 2017 from http://cse.ffpri.affrc.go.jp/shojih/data/index.html
+ncfiles <- c("~/Data/Hashimoto/RH_yr_Hashimoto2015.nc",
+             "~/Data/Hashimoto/RS_yr_Hashimoto2015.nc")
+
+for(ncfile in ncfiles) {
+  printlog(SEPARATOR)
+  printlog("Reading", ncfile)
+  nc <- nc_open(ncfile)
+  
+  #co2 <- ncvar_get(nc, "co2", start = c(199, 299, 1, 90), count = c(3, 3, 1, 22))
+  #co2[2,2,] <- NA # punch a hole for testing
+  
+  # These annual data start in 1901; extract 1990-2012
+  co2 <- ncvar_get(nc, "co2", start = c(1, 1, 1, 90), count = c(-1, -1, 1, 23))
+  printlog("Dimensions =", paste(dim(co2), collapse = " "))
+  
+  # Fit a linear model (i.e. over time) to each grid cell
+  f <- function(rh) { 
+    df <- data.frame(x = seq_along(rh), y = rh)
+    m <- tryCatch(lm(y ~ x, data = df), 
+                  error = function(e) NA)
+  }
+  printlog("Fitting linear model to each grid cell (this is slow)...")
+  mods <- apply(co2, c(1, 2), FUN = f)  # slow
+  
+  printlog("Extracting slopes...")
+  slopes <- apply(mods, c(1, 2), FUN = function(x) 
+    if(!is.na(x)) x[[1]]$coefficients[["x"]] else NA)
+  slopes <- matrix(slopes, nrow = nrow(mods), ncol = ncol(mods))
+  
+  printlog("Extracting slope p-values...")
+  signif <- apply(mods, c(1, 2), FUN = function(x) 
+    if(!is.na(x)) summary(x[[1]])$coefficients["x", "Pr(>|t|)"] else NA)
+  signif <- matrix(signif, nrow = nrow(mods), ncol = ncol(mods))
+  
+  image(signif)
+  hist(signif)
+  
+  ncells <- sum(!is.na(slopes))
+  printlog("Total cells =", ncells)
+  pos_slope <- sum(slopes > 0, na.rm = TRUE)
+  printlog("Cells with positive slope =", pos_slope,
+           "or", round(pos_slope / ncells * 100, 0), "%")
+  signif_pos_slope <- sum(slopes > 0 & signif < 0.05, na.rm = TRUE)
+  printlog("Cells with significant positive slope =", signif_pos_slope, 
+           "or", round(signif_pos_slope / ncells * 100, 0), "%")
+}
+
+printlog("All done with", SCRIPTNAME)
+closelog()
+
+if(PROBLEM) warning("There was a problem - see log")
+
