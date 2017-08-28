9922a851c02596df8ad15bf3edda57b961d42698
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-08-28 16:45:02 -0400

    Add final faceted data graph

--- X-rev2_gppsif.R
+++ X-rev2_gppsif.R
@@ -28,11 +28,12 @@ yrs <- 1990:2014
 
 rs_adjustment_25yr <- 0.1  # remote sensing missing 10% (half)
 
+all_data <- list()
 results <- list()
 for(rs_adjustment_25yr in seq(0, 0.9, by = 0.1)) {
   printlog(SEPARATOR)
   
-  base <- tibble(year = yrs, gpp = rep(1, length.out = length(yrs)))
+  base <- tibble(year = yrs, gpp = rep(1, length.out = length(yrs)), sat_missing = rs_adjustment_25yr)
   
   base %>%
     mutate(GPPSIF = "GPP[MODIS]",
@@ -65,7 +66,7 @@ for(rs_adjustment_25yr in seq(0, 0.9, by = 0.1)) {
   save_plot(paste("gpp_adjustment", rs_adjustment_25yr, sep = "_"))
   
   adjustment_data %>% 
-    dplyr::select(year, GPPSIF, adjust) %>% 
+    dplyr::select(year, GPPSIF, adjust, sat_missing) %>% 
     right_join(s_gppsif, by = c("GPPSIF", "year" = "year_round")) ->
     s_gppsif_adj
   
@@ -74,13 +75,18 @@ for(rs_adjustment_25yr in seq(0, 0.9, by = 0.1)) {
   for(dataset in unique(s_gppsif_adj$GPPSIF)) {
     for(f in unique(s_gppsif_adj$Flux)) {
       d <- filter(s_gppsif_adj, Flux == f, GPPSIF == dataset, !is.na(mat_hadcrut4), !is.na(map_hadcrut4))
-      dname <- make.names(paste(dataset, f, rs_adjustment_25yr))
+      dname <- make.names(paste("gppadjust", dataset, f, rs_adjustment_25yr))
       
-      # The 'adjust' values apply to the *increase*, not the absolute GPP
-      # Compute trend
+      # The 'adjust' values (how much GPP is hypothetically missed by satellites
+      # apply to the *increase* in, not the absolute value of, GPP
+      # So, compute the trend
+      stop()
       trend <- coefficients(lm(gppsifvalue ~ Study_midyear, data = d))["Study_midyear"]
-      # Compute an adjusted GPP, which is trend * years * adjustment added to original GPP
+      # Compute an adjusted GPP, which is GPP + trend (/yr) * years * adjustment
       d$gppsif_adjusted <- d$gppsifvalue + (d$Study_midyear - min(d$Study_midyear)) * trend * d$adjust
+      
+      # Save for later use
+      all_data[[dname]] <- d
       
       # Linear model
       m <- lm(fluxvalue / gppsif_adjusted ~ Study_midyear, data = d)
@@ -100,14 +106,31 @@ for(rs_adjustment_25yr in seq(0, 0.9, by = 0.1)) {
 
 printlog(SEPARATOR)
 results <- bind_rows(results)
+results$signed_p_value <- results$p * sign(results$trend)
 
-p <- ggplot(filter(results, trend >= 0), aes(rs_adjustment_25yr, p, color = dataset)) +
+p <- ggplot(results, aes(rs_adjustment_25yr, signed_p_value, color = dataset)) +
   geom_line() + facet_grid(flux ~ ., scales = "free") +
   xlab("Fraction of GPP missed by satellites") + 
   ylab("p-value for trend of R/GPPadjusted") +
   geom_hline(yintercept = 0.05, linetype = 2)
 print(p)
 save_plot("satellite_missing_gpp")
+
+all_data <- bind_rows(all_data)
+
+for(dataset in unique(s_gppsif_adj$GPPSIF)) {
+  for(f in unique(s_gppsif_adj$Flux)) {
+    d <- filter(all_data, GPPSIF == dataset, Flux== f)
+    dname <- make.names(paste("gppadjust", dataset, f))
+    
+    p <- qplot(Study_midyear, fluxvalue / gppsif_adjusted, data = d) + 
+      geom_smooth(method = "lm") + facet_wrap(~sat_missing) +
+      ggtitle(dname) + ylim(c(0, 2))
+    print(p)
+    save_plot(dname)
+  }
+}
+
 
 printlog("All done with", SCRIPTNAME)
 closelog()
